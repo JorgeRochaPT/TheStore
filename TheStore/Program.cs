0794c80e06b4e621ecfbb2110ba2b4e7c305f48c
Jorge Rocha <jorge.rocha@improxy.com>
2021-09-20 14:31:09 +0100

    Add project files.

---
+++ TheStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
