43bcb6e3e0e3fd7ce09c3bc313c186cced74fbf6
Pedro <pedropinto1994@gmail.com>
2021-09-20 16:15:22 +0100

    Classe cliente feita
    ola

--- TheStore/Cliente.cs
+++ TheStore/Cliente.cs
@@ -4,10 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TheStore
-{
-    class Cliente
+        namespace TheStore
     {
-        //ola Jose
+        class Cliente
+        {
+            public string Nome { get; set; }
+
+            public string Morada { get; set; }
+
+            public string Email { get; set; }
+
+            public int Nif { get; set; }
+
+            public int Telefone { get; set; }
+
+        }
+         class cartao : Cliente
+        {
+
+            public int IDcartao { get; set; }
+
+            public int Pontos { get; set; }
+
+            public double SaldoTotal { get; set; }
+
+        }
     }
-}
+
